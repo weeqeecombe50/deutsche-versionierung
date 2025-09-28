88e3f22ce8348724174dcd4614e12791be3331cb
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-28 16:36:24 +0300

    Added a help command to cli.js for better user guidance.

--- cli.js
+++ cli.js
@@ -7,6 +7,11 @@ if (command === 'start') {
     // Add more commands for versioning here.
 } else if (command === 'stop') {
     console.log('Versioning has stopped.');
+} else if (command === 'help') {
+    console.log('Available commands:');
+    console.log('- start: Start versioning');
+    console.log('- stop: Stop versioning');
+    console.log('- help: Show this help message');
 } else {
     console.log('Unknown command:', command);
 }
