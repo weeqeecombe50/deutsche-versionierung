705c938107744e32f75ae7ea2527aba39cb0c5f4
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-28 16:38:32 +0300

    Add a greeting for users in different languages.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log('Welcome to the program! Various language versions are available.');
-
-// Further logic for the main program will be added here.
+console.log('¡Bienvenido al programa! Varias versiones en diferentes idiomas están disponibles.');
+console.log('Bienvenue dans le programme! Plusieurs versions linguistiques sont disponibles.');
