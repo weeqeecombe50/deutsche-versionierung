de77fd03806640bd57717a24459a4217f3bebea0
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-28 16:37:11 +0300

    Added a 'status' command to check versioning state.

--- cli.js
+++ cli.js
@@ -7,10 +7,13 @@ if (command === 'start') {
     // Add more commands for versioning here.
 } else if (command === 'stop') {
     console.log('Versioning has stopped.');
+} else if (command === 'status') {
+    console.log('Versioning is currently running.');
 } else if (command === 'help') {
     console.log('Available commands:');
     console.log('- start: Start versioning');
     console.log('- stop: Stop versioning');
+    console.log('- status: Check versioning state');
     console.log('- help: Show this help message');
 } else {
     console.log('Unknown command:', command);
